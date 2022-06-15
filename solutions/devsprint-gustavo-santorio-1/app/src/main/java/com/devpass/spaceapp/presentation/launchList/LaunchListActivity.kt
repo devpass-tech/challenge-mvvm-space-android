bfdb537f1c03972cfe0d0515efa5496f9366139d
Rodrigo Borges <rborges.soares@gmail.com>
2022-06-15 16:29:11 -0300

    Base project for Dev Sprint Gustavo Santorio #1 (by Impulso)

---
+++ solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/presentation/launchList/LaunchListActivity.kt
@@ -0,0 +1,40 @@
+package com.devpass.spaceapp.presentation.launchList
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.devpass.spaceapp.databinding.ActivityLaunchListBinding
+import com.devpass.spaceapp.R
+
+class LaunchListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLaunchListBinding
+
+    private lateinit var adapter: LaunchListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLaunchListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecycleView()
+        initLaunchList()
+    }
+
+    private fun initLaunchList() {
+
+        val launch1 = LaunchModel("Launch 1","1", "July 03, 2020", "Success", R.drawable.crs)
+        val launch2 = LaunchModel("Launch 2","2", "July 03, 2020", "Success", R.drawable.falcon_sat)
+        val launch3 = LaunchModel("Launch 3","3", "July 03, 2020", "Success", R.drawable.starlink)
+        val launch4 = LaunchModel("Launch 4","4", "July 03, 2020", "Success", R.drawable.spacex_dragon_crs20_patch01)
+        val launch5 = LaunchModel("Launch 5","5", "July 03, 2020", "Success", R.drawable.starlink)
+
+        var launchList: List<LaunchModel> = listOf(launch1, launch2, launch3, launch4, launch5)
+        adapter.submitList(launchList)
+    }
+
+    private fun setupRecycleView() {
+        adapter = LaunchListAdapter()
+        binding.rvLaunches.adapter = adapter
+        binding.rvLaunches.layoutManager = LinearLayoutManager(this)
+    }
+}
