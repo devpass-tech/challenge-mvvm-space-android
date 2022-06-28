736297e252f57fd892e42552eff3570da3e3eaec
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2022-06-28 20:11:07 -0300

    Create viewModel

--- solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/presentation/RocketDetailsViewModel.kt
+++ solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/presentation/RocketDetailsViewModel.kt
@@ -1,4 +1,22 @@
 package com.devpass.spaceapp.rocketDetails.presentation
 
-class RocketDetailsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.devpass.spaceapp.rocketDetails.domain.RocketDetailsUseCase
+import com.devpass.spaceapp.rocketDetails.domain.RocketDetailsUseCaseImpl
+import kotlinx.coroutines.launch
+
+class RocketDetailsViewModel(
+    private val useCase: RocketDetailsUseCase = RocketDetailsUseCaseImpl()):ViewModel() {
+     init {
+            getDetails()
+        }
+    private fun getDetails(){
+        viewModelScope.launch {
+            useCase.getDetails().collect{
+                //criar livedata ou stateflow, atribuir valor e consumir na tela
+
+            }
+        }
+    }
 }
