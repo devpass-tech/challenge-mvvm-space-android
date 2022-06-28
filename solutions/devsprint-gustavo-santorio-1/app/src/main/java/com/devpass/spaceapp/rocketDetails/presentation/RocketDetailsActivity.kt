9d93901c11ad6f0e11bec608815caaf581e7dac6
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2022-06-28 20:09:59 -0300

    Implement viewModel in activity

--- solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/presentation/RocketDetailsActivity.kt
+++ solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/presentation/RocketDetailsActivity.kt
@@ -2,12 +2,16 @@ package com.devpass.spaceapp.rocketDetails.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.devpass.spaceapp.R
 
 class RocketDetailsActivity : AppCompatActivity() {
+    private val viewModel by viewModels<RocketDetailsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rocket_details)
+
     }
 
 }
