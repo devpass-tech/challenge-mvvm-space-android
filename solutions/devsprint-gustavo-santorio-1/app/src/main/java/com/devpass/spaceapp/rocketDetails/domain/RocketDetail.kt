e3117ad4aa9b9e8fc0b4b0c7256025e910b36b9c
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2022-06-28 20:08:28 -0300

    create Rocket Detail Model

--- solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/domain/RocketDetail.kt
+++ solutions/devsprint-gustavo-santorio-1/app/src/main/java/com/devpass/spaceapp/rocketDetails/domain/RocketDetail.kt
@@ -1,3 +1,7 @@
 package com.devpass.spaceapp.rocketDetails.domain
 
-data class RocketDetail()
+data class RocketDetail(
+    val name: String,
+    val description: String,
+    val image: String
+)
