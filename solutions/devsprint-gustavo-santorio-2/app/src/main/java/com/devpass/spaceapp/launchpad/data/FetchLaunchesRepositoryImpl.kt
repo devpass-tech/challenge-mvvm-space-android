6eb81289f5a3d1b974a1e6e05fc236b9dda74fd0
Pedro Pinheiro <84847982+PedroPinheiroDev@users.noreply.github.com>
2022-07-14 18:04:30 -0300

    Update solutions/devsprint-gustavo-santorio-2/app/src/main/java/com/devpass/spaceapp/launchpad/data/FetchLaunchesRepositoryImpl.kt
    Co-authored-by: GustavoHSSantorio <gustavohss13@gmail.com>

--- solutions/devsprint-gustavo-santorio-2/app/src/main/java/com/devpass/spaceapp/launchpad/data/FetchLaunchesRepositoryImpl.kt
+++ solutions/devsprint-gustavo-santorio-2/app/src/main/java/com/devpass/spaceapp/launchpad/data/FetchLaunchesRepositoryImpl.kt
@@ -8,7 +8,20 @@ import com.devpass.spaceapp.launchpad.domain.model.LaunchpadVO
 class FetchLaunchesRepositoryImpl(
     private val service: SpaceXAPIService = NetworkModule.retrofitInstance.create(SpaceXAPIService::class.java)
 ) : FetchLaunchesRepository {
-    override suspend fun fetchLaunchpadDetails(id: String): Result<LaunchpadVO> {
+    override suspend fun fetchLaunchpadDetails(id: String): Result<LaunchpadVO> {    
+           return runCatching {
+             val response = service.fetchLaunchpadDetails(id = id)
+             with(response) {
+                 LaunchpadVO(
+                     name = name,
+                     fullName = full_name,
+                     region = region,
+                     launchAttempts = launch_attempts.toString(),
+                     launchSuccesses = launch_successes.toString()
+                 )
+             }
+         }
+     }
         return runCatching {
             val response = service.fetchLaunchpadDetails(id = id)
             with(response) {
