824e2814aa04fd0e2151b38634343e4668b7f5fb
Ingrid da Silva Oliveira <ingrid.oliveiratk@gmail.com>
2022-08-04 19:27:07 -0300

    implementation api details

--- solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIService.kt
+++ solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIService.kt
@@ -1,10 +1,17 @@
 package com.devpass.spaceapp.data.api
 
+import com.devpass.spaceapp.model.Launchpad
+import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface SpaceXAPIService {
 
     @POST("v5/launches/query")
     suspend fun fetchNextLaunches(@Body params: QueryRequestParams): NextLaunchesModel
+
+    @GET("v4/launchpads/{id}")
+    suspend fun fetchLaunchpadDetails(@Path("id") id: String): Call<Launchpad>
 }
--- solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/repository/FetchLaunchesRepository.kt
+++ solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/repository/FetchLaunchesRepository.kt
@@ -1,7 +1,11 @@
 package com.devpass.spaceapp.repository
 
 import com.devpass.spaceapp.data.api.NextLaunchesModel
+import com.devpass.spaceapp.model.Launchpad
+import com.devpass.spaceapp.model.repository.FechLaunchpadImpl
 
 interface FetchLaunchesRepository {
     suspend fun fetchLaunches(): NextLaunchesModel
+
+    suspend fun fetchLaunchpad(): Launchpad
 }
