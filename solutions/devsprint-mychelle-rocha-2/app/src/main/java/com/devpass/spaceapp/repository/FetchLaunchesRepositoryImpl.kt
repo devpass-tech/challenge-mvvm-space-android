5e43c8893b9a28874654cdc03537ddb7830a7b07
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-08-08 19:44:33 -0300

    implemented fetchLaunchDetails

--- solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/repository/FetchLaunchesRepositoryImpl.kt
+++ solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/repository/FetchLaunchesRepositoryImpl.kt
@@ -1,15 +1,14 @@
-import com.devpass.spaceapp.data.api.*
+import com.devpass.spaceapp.data.api.NextLaunchesModel
+import com.devpass.spaceapp.data.api.OptionsRequest
+import com.devpass.spaceapp.data.api.QueryRequestParams
+import com.devpass.spaceapp.data.api.SpaceXAPIService
 import com.devpass.spaceapp.repository.FetchLaunchesRepository
 
 private const val NUMBER_ROCKETS = 20
 
 class FetchLaunchesRepositoryImpl(private val api: SpaceXAPIService) : FetchLaunchesRepository {
-    override suspend fun fetchLaunches(): Results<NextLaunchesModel> {
-        return try {
-            Results.Success(api.fetchNextLaunches(getParams()))
-        } catch (exception: Exception) {
-            Results.Error(exception)
-        }
+    override suspend fun fetchLaunches(): NextLaunchesModel {
+        return api.fetchNextLaunches(getParams())
     }
 
     private fun getParams() =
