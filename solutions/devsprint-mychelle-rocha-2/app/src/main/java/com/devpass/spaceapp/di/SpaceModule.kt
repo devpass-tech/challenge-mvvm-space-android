8d60ba73ff304d22e85e41c331b8f6a116cfdfcd
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-08-31 16:49:58 -0300

    implemented rocketFragment to rocketDetailsViewModel

--- solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/di/SpaceModule.kt
+++ solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/di/SpaceModule.kt
@@ -1,17 +1,15 @@
 package com.devpass.spaceapp.di
 
 import FetchLaunchesRepositoryImpl
-import com.devpass.spaceapp.data.api.NetworkModule
 import com.devpass.spaceapp.data.api.SpaceXAPIService
 import com.devpass.spaceapp.model.repository.FetchRocketRepositoryImpl
-import com.devpass.spaceapp.presentation.RocketDetailsViewModel
+import com.devpass.spaceapp.presentation.rocket.RocketDetailsViewModel
 import com.devpass.spaceapp.presentation.launchList.LaunchListViewModel
 import com.devpass.spaceapp.repository.FetchLaunchesRepository
 import com.devpass.spaceapp.repository.FetchRocketRepository
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.create
 
 val spaceModules = module{
     factory<FetchLaunchesRepository>{FetchLaunchesRepositoryImpl(get())}
@@ -19,6 +17,6 @@ val spaceModules = module{
     viewModel{LaunchListViewModel(get())}
 
     factory<FetchRocketRepository>{FetchRocketRepositoryImpl(get())}
-    viewModel{RocketDetailsViewModel(get())}
+    viewModel{ RocketDetailsViewModel(get()) }
 
 }
--- solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/presentation/rocket/RocketDetailsViewModel.kt
+++ solutions/devsprint-mychelle-rocha-2/app/src/main/java/com/devpass/spaceapp/presentation/rocket/RocketDetailsViewModel.kt
@@ -1,10 +1,11 @@
-package com.devpass.spaceapp.presentation
+package com.devpass.spaceapp.presentation.rocket
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.devpass.spaceapp.data.api.Results
 import com.devpass.spaceapp.model.Rocket
+import com.devpass.spaceapp.presentation.StateClassView
 import com.devpass.spaceapp.presentation.launchList.StateViewData
 import com.devpass.spaceapp.repository.FetchRocketRepository
 import kotlinx.coroutines.launch
