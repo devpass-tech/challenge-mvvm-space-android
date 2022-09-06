5adcf4b13734cd838eacbd3b72c3842d1ea2e6a9
Victor Montero <victorhugo.montero@thomsonreuters.com>
2022-09-06 14:04:10 -0300

    "removing unused import"

--- solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/presentation/FragmentDetails.kt
+++ solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/presentation/FragmentDetails.kt
@@ -2,15 +2,13 @@ package com.devpass.spaceapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.devpass.spaceapp.databinding.FragmentDetailsBinding
-import com.google.android.material.snackbar.Snackbar
 
-class FragmentDetails: Fragment() {
+class FragmentDetails : Fragment() {
 
     private var binding: FragmentDetailsBinding? = null
 
