2011c045978de09fb5f0f2516bf7322351f32c51
isabella-cruz <isabella.cruz@ifood.com.br>
2022-09-08 17:05:01 -0300

    Create API integration to last launches

--- solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIService.kt
+++ solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIService.kt
@@ -1,5 +1,13 @@
 package com.devpass.spaceapp.data.api
 
+import com.devpass.spaceapp.data.api.response.LaunchesPageResponse
+import com.devpass.spaceapp.data.api.response.QueryParams
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface SpaceXAPIService {
+
+    @POST("v5/launches/query")
+    suspend fun fetchNextLaunches(@Body params: QueryParams): LaunchesPageResponse
 
 }
