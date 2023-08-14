c37476f95867823fc2e7a2e243647aabb3ac7258
joycesouzs <joycejfs@hotmail.com>
2023-08-14 16:42:39 -0300

    add service instance

--- solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/retrofit/RetrofitClient.kt
+++ solutions/devsprint-haldny-santos-1/app/src/main/java/com/devpass/spaceapp/data/retrofit/RetrofitClient.kt
@@ -1,15 +1,18 @@
 package com.devpass.spaceapp.data.retrofit
 
+import com.devpass.spaceapp.data.api.SpaceXAPIService
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://api.spacexdata.com/latest"
 
-    val retrofit: Retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
     }
+
+    fun getSpaceXAPI() = retrofit.create(SpaceXAPIService::class.java)
 }
